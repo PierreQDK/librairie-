9b43e3f8b27328a1f0d717c1643c28dea892def4
PierreQDK <pierre.quintindekercadio@gmail.com>
2025-02-24 15:12:46 +0100

    Nettoyage complet de la branche gh-pages

--- LibrairieQDK/tests/testthat/test-compter_nombre_d_adjoints.R
+++
@@ -1,9 +0,0 @@
-test_that("compter_nombre_d_adjoints fonctionne avec une entrée valide", {
-  df <- data.frame(
-    Libellé.de.la.fonction = c("Maire-adjoint", "Adjoint", "Conseiller", "adjoint"),
-    stringsAsFactors = FALSE
-  )
-  df <- cbind(df, matrix(ncol = 15, nrow = 4))  # S'assurer qu'il y a 16 colonnes
-  result <- compter_nombre_d_adjoints(df)
-  expect_equal(result, 2)  # Vérifier que le résultat est bien 2
-})
--- LibrairieQDK/tests/testthat/test-plot_code_professions.R
+++
@@ -1,9 +0,0 @@
-test_that("plot_code_professions fonctionne sans erreur", {
-  df <- data.frame(
-    Libellé.de.la.fonction = c("Maire", "Maire-adjoint", "Maire", "Adjoint"),
-    Code.de.la.catégorie.socio.professionnelle = c("A", "B", "C", "D"),
-    stringsAsFactors = FALSE
-  )
-  df <- cbind(df, matrix(ncol = 14, nrow = 4))  # Pour arriver à 16 colonnes
-  expect_invisible(plot_code_professions(df))
-})
